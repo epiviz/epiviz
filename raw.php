bc67ca8cba71bfe9d43a3fca388cfe0a90b98186
Hector Corrada Bravo <hcorrada@gmail.com>
2014-09-14 13:39:41 -0400

    remove uneeded files after minimizing

--- raw.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if (array_key_exists('url', $_REQUEST) && filter_var($_REQUEST['url'], FILTER_VALIDATE_URL)) {
-  header('Content-type: application/javascript');
-  readfile($_REQUEST['url']);
-}
