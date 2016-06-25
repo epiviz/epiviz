8d112cc2878355ba6bb53de5c4f661fe2db4ff6a
Hector Corrada Bravo <hcorrada@gmail.com>
2016-06-25 11:32:24 -0700

    change websocket path construction for epivizr

--- src/epiviz/epivizr-settings.js
+++ src/epiviz/epivizr-settings.js
@@ -3,9 +3,15 @@
  * on 4/22/16.
  */
 
+var websocket_path = window.location.pathname.split(',');
+websocket_path.pop();
+websocket_path = window.location.host + websocket_path.join('/') + "/websocket";
+
+websocket_host = window.location.host;
 epiviz.Config.SETTINGS.dataProviders.push(
     sprintf('epiviz.data.WebsocketDataProvider,%s,%s',
         epiviz.data.WebsocketDataProvider.DEFAULT_ID,
-        sprintf("ws://%s", window.location.host)));
+        sprintf("ws://%s", websocket_path)));
+
 
 epiviz.Config.SETTINGS.configType = "epivizr_standalone";
