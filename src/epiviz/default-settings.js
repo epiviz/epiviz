3550c39f35b1ee57df1636965120013a718b27b9
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2016-08-10 11:41:51 -0400

    rename colorbrewer names

--- src/epiviz/default-settings.js
+++ src/epiviz/default-settings.js
@@ -311,9 +311,9 @@ epiviz.Config.SETTINGS = {
       'Set3', 'colorBrewer-Set3-default'),
     new epiviz.ui.charts.ColorPalette(
       ["#8e0152","#c51b7d","#de77ae","#f1b6da","#fde0ef","#f7f7f7","#e6f5d0","#b8e186","#7fbc41","#4d9221","#276419"],
-      'Set3', 'colorBrewer-PiYG'),
+      'PiYG', 'colorBrewer-PiYG'),
     new epiviz.ui.charts.ColorPalette(
       ["#40004b","#762a83","#9970ab","#c2a5cf","#e7d4e8","#f7f7f7","#d9f0d3","#a6dba0","#5aae61","#1b7837","#00441b"],
-      'Set3', 'colorBrewer-PRGn')
+      'PRGn', 'colorBrewer-PRGn')
   ]
 };
