3fca57c54e68e5aeedf83b3591abebf90eae2a66
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2017-02-13 13:38:43 -0500

    update site-settings for current datasets

--- site-settings.js
+++ site-settings.js
@@ -4,24 +4,60 @@ epiviz.Config.SETTINGS.dataServerLocation = 'http://metaviz.cbcb.umd.edu/data/';
 // This sets up the UMD data server as the only data provider
 epiviz.Config.SETTINGS.dataProviders = [
     [
-      // fully qualified class name for the class
+      // dataprovider Class Name 
       'epiviz.data.EpivizApiDataProvider',
-
-      // the name of the datasource (matching the datasource in the UI add measurements dialog)
-      'hmp',
-
-      // where the api is located, relative to dataServerLocation (see above)
-      'hmp-api/',
-
-      // retrieve only this measurement annotation:
-      ['sex', 'hmpbodysubsite', 'hmpbodysupersite', 'sampcollectdevice', 'envmatter', 'bodysite', 'bmi', 'visitno', 'age', 'runcenter', 'runid', 'description'],
-
-      // this is the initial depth of icicles:
+      // datasource name
+      'msd16s',
+      //dataserver location, where api requests go to.
+      'api/',
+      // measurement annotations
+      [],
+      // initial depth of icicle
       3,
-
-      // aggregate at these levels in the tree:
-      {3: epiviz.ui.charts.tree.NodeSelectionType.NODE, 4: epiviz.ui.charts.tree.NodeSelectionType.NODE}
+      // initial aggregation level
+      {3: epiviz.ui.charts.tree.NodeSelectionType.NODE}
+    ],
+    [
+      'epiviz.data.EpivizApiDataProvider',
+      'etec16s',
+      'api/',
+      [],
+      3,
+      {3: epiviz.ui.charts.tree.NodeSelectionType.NODE}
+    ],
+    [
+      'epiviz.data.EpivizApiDataProvider',
+      'tbi_mouse',
+      'api/',
+      [],
+      3,
+      {3: epiviz.ui.charts.tree.NodeSelectionType.NODE}
+    ],
+    [
+      'epiviz.data.EpivizApiDataProvider',
+      'hmp',
+      'api/',
+      [],
+      3,
+      {3: epiviz.ui.charts.tree.NodeSelectionType.NODE}
+    ],
+    [
+      'epiviz.data.EpivizApiDataProvider',
+      'igs_test',
+      'api/',
+      [],
+      3,
+      {3: epiviz.ui.charts.tree.NodeSelectionType.NODE}
+    ],
+    [
+      'epiviz.data.EpivizApiDataProvider',
+      'ihmp_data',
+      'api/',
+      [],
+      3,
+      {3: epiviz.ui.charts.tree.NodeSelectionType.NODE}
     ]
+
 ];
 
 // This sets up the UMD workspace server 
