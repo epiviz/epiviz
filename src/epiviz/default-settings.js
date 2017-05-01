899e7d88b71dda4d3a756244da5aeb9ba54d760b
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2017-05-01 10:56:07 -0400

    update heatmap colors

--- src/epiviz/default-settings.js
+++ src/epiviz/default-settings.js
@@ -274,7 +274,8 @@ epiviz.Config.SETTINGS = {
       ['#f9a65a', '#599ad3', '#79c36a', '#f1595f', '#727272', '#cd7058', '#d77fb3'],
       'Genes Default', 'genes-default'),
     new epiviz.ui.charts.ColorPalette(
-      ['#1859a9', '#ed2d2e', '#008c47', '#010101', '#f37d22', '#662c91', '#a11d20', '#b33893'],
+      //['#1859a9', '#ed2d2e', '#008c47', '#010101', '#f37d22', '#662c91', '#a11d20', '#b33893'],
+      ['#1859a9', '#ff7f0e', '#2ca02c', '#010101', '#f37d22', '#662c91', '#a11d20', '#b33893'],
       'Heatmap Default', 'heatmap-default'),
     new epiviz.ui.charts.ColorPalette(
       ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"],
