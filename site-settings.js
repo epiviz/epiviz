0788562575119ac4c20d5e6141f7f2bcd0ed719a
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2018-08-03 09:14:56 -0400

    add dependencies

--- site-settings.js
+++ site-settings.js
@@ -1,19 +1,19 @@
 // This uses the UMD server for login and user management
-epiviz.Config.SETTINGS.dataServerLocation = 'http://epiviz.cbcb.umd.edu/data/';
+epiviz.Config.SETTINGS.dataServerLocation = "http://epiviz.cbcb.umd.edu/data/";
 
 // This sets up the UMD data server as the only data provider
 epiviz.Config.SETTINGS.dataProviders = [
-    [
-        // fully qualified class name for the class
-        'epiviz.data.WebServerDataProvider',
-        epiviz.Config.DEFAULT_DATA_PROVIDER_ID,
-        // 'http://epiviz.cbcb.umd.edu/data/main.php'
-        'http://localhost:5000/'
-
-    ]
+  [
+    // fully qualified class name for the class
+    "epiviz.data.WebServerDataProvider",
+    "umd",
+    "http://epiviz-dev.cbcb.umd.edu/api/"
+  ]
 ];
 
-// This sets up the UMD workspace server 
-epiviz.Config.SETTINGS.workspacesDataProvider = sprintf('epiviz.data.WebServerDataProvider,%s,%s',
-    'workspaces_provider',
-    'http://epiviz.cbcb.umd.edu/data/main.php');
+// This sets up the UMD workspace server
+epiviz.Config.SETTINGS.workspacesDataProvider = sprintf(
+  "epiviz.data.WebServerDataProvider,%s,%s",
+  "workspaces_provider",
+  "http://epiviz.cbcb.umd.edu/data/main.php"
+);
